6e1b3c648bc9335b641b4129c0b1f0ce96909a72
tarikisildar <mtarik1907@gmail.com>
2023-10-06 15:16:32 +0200

    Add Error Handling

--- OpenGLTutorial/Application.cpp
+++ OpenGLTutorial/Application.cpp
@@ -4,6 +4,30 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <cassert>
+
+#define assertm(exp, msg) assert(((void)msg, exp))
+#define GLCALL(x) GLClearError();\
+                  x;\
+                  assert(GLCheckError(#x, __FILE__, __LINE__));
+
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLCheckError(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "OpenGL Error: (" << error << ")"
+            << function << " " << file << " " << line
+            << std::endl;
+        return false;
+    }
+    return true;
+}
 
 struct ShaderProgramSource
 {
@@ -148,13 +172,13 @@ int main(void)
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
+        GLCALL(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        
         /* Swap front and back buffers */
-        glfwSwapBuffers(window); 
+        GLCALL(glfwSwapBuffers(window));
 
         /* Poll for and process events */
-        glfwPollEvents();
+        GLCALL(glfwPollEvents());
     }
 
     glDeleteProgram(shader);
